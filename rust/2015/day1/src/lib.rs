11a419f6880d94ecb51c15e619f8b143ea003f71
Ryan Sadowski <nemosaurus@gmail.com>
2023-06-04 21:22:03 -0700

    2 gold stars

--- rust/2015/day1/src/lib.rs
+++ rust/2015/day1/src/lib.rs
@@ -1,6 +1,38 @@
-pub fn process_part1(input: &str) -> String {
-    "works".to_string()
+pub fn process_part1(input: &str) -> i32 {
+    let mut floorz : i32 = 0;
+    // https://www.ascii-code.com
+    //why can I do byes but not char?
+    for c in input.bytes() {
+        if c == 41 {
+            println!(" 🔽 ");
+            floorz -= 1;
+        } else if c == 40 {
+            println!(" 🔺 ");
+            floorz += 1; 
+        }
+    }
+    return floorz; 
+}
 
+pub fn process_part2(input: &str) -> i32 {
+    let mut floorz : i32 = 0;
+    let mut count : i32 = 0;
+    // https://www.ascii-code.com
+    //why can I do byes but not char?
+    for c in input.bytes() {
+        count += 1;
+        if c == 41 {
+            println!(" 🔽 ");
+            floorz -= 1;
+        } else if c == 40 {
+            println!(" 🔺 ");
+            floorz += 1; 
+        }
+        if floorz < 0 {
+            return count;
+        }
+    }
+    return floorz;    
 }
 
 #[cfg(test)]
@@ -9,7 +41,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = process_part1("asdf");
-        assert_eq!(result, "works");
+        let result = process_part1("(())");
+        assert_eq!(result, 0);
     }
 }
