c47af65fecdf5b64dfbfb3a7f35793bcf2654a44
RyanSadowski <sadowskiryan@gmail.com>
2023-07-19 13:05:50 -0700

    🙃

--- rust/2015/day6/src/lib.rs
+++ rust/2015/day6/src/lib.rs
@@ -19,7 +19,38 @@ pub fn process_part1(input: Vec<&str>) -> bool {
 
 }
 
-fn parseCommand(input: &str) {
+
+enum Command{
+    on,
+    off,
+    toggle
+}
+
+struct LightCommand {
+    //toggle, on, off
+    // !, 1 , 0
+    cmd: Command,
+    minX: i32,
+    maxX: i32,
+    minY: i32,
+    maxY: i32
+
+}
+
+fn parseCommand(input: &str) -> Option<LightCommand> {
     
+}
+
+
+fn getCommandEnum(input: &str) -> Command {
+    if(input.contains("turn off")){
+        Command::off
+    } else if (input.contains("turn on")){
+        Command::on
+    } else if (input.contains("toggle")){
+        Command::toggle
+    } else {
+        Command::toggle
+    }
 }
 
