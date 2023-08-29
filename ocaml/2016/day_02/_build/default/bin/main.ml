15fe3e98ac92646ec5d58212bae90b2ab4b8816f
Ryan Sadowski <ryan.sadowski@knighttrans.com>
2023-08-28 20:26:55 -0700

    🙃

--- ocaml/2016/day_02/_build/default/bin/main.ml
+++ ocaml/2016/day_02/_build/default/bin/main.ml
@@ -9,11 +9,28 @@ let keypad = [|
         [|7; 8; 9|]
         |]
 
-let is_valid_position x y = 
-        x >= 0 && x < Array.length keypad && y >= 0 && y < Array.length keypad.(0)
+type position = {x: int; y: int}
 
-let update_pos currentPosition char = 
-        currentPosition
+let is_valid_position pos = 
+        pos.x >= 0 && pos.x < Array.length keypad && pos.y >= 0 && pos.y < Array.length keypad.(0)
+
+let update_pos currentPosition char =
+        (* let () = printf "processing %c \n" char in  *)
+        match char with 
+        | 'U' -> 
+                if currentPosition.y > 0 then {x = currentPosition.x; y = currentPosition.y - 1 }
+                else currentPosition
+        | 'D' -> 
+                if currentPosition.y < 2 then {x = currentPosition.x; y = currentPosition.y + 1 }
+                else currentPosition
+        | 'L' -> 
+                if currentPosition.x > 0 then {x = currentPosition.x - 1; y = currentPosition.y }
+                else currentPosition
+        | 'R' -> 
+                if currentPosition.x < 2 then {x = currentPosition.x + 1; y = currentPosition.y }
+                else currentPosition
+        | _ -> failwith "rip 🪦"
+
 
 let rec process line currentPosition =
         match String.length line with
@@ -36,6 +53,10 @@ let () =
         if Array.length argv < 2 then
                 print_endline "Please provide a filename"
         else
-                let filename = argv.(1) in
-                let  lines = read_file_to_lines filename in
-                List.iter ~f:(printf "%s\n") lines
+        let filename = argv.(1) in
+        let lines = read_file_to_lines filename in
+        let cur_pos = {x=1; y=1} in
+        List.iter lines ~f:(fun line -> 
+            let result_pos = process line cur_pos in
+            printf "(%d, %d) = %d\n" result_pos.x result_pos.y keypad.(result_pos.y).(result_pos.x)
+        )
--- ocaml/2016/day_02/bin/main.ml
+++ ocaml/2016/day_02/bin/main.ml
@@ -14,8 +14,23 @@ type position = {x: int; y: int}
 let is_valid_position pos = 
         pos.x >= 0 && pos.x < Array.length keypad && pos.y >= 0 && pos.y < Array.length keypad.(0)
 
-let update_pos currentPosition char = 
-        currentPosition
+let update_pos currentPosition char =
+        (* let () = printf "processing %c \n" char in  *)
+        match char with 
+        | 'U' -> 
+                if currentPosition.y > 0 then {x = currentPosition.x; y = currentPosition.y - 1 }
+                else currentPosition
+        | 'D' -> 
+                if currentPosition.y < 2 then {x = currentPosition.x; y = currentPosition.y + 1 }
+                else currentPosition
+        | 'L' -> 
+                if currentPosition.x > 0 then {x = currentPosition.x - 1; y = currentPosition.y }
+                else currentPosition
+        | 'R' -> 
+                if currentPosition.x < 2 then {x = currentPosition.x + 1; y = currentPosition.y }
+                else currentPosition
+        | _ -> failwith "rip 🪦"
+
 
 let rec process line currentPosition =
         match String.length line with
@@ -43,5 +58,5 @@ let () =
         let cur_pos = {x=1; y=1} in
         List.iter lines ~f:(fun line -> 
             let result_pos = process line cur_pos in
-            printf "(%d, %d)\n" result_pos.x result_pos.y
+            printf "(%d, %d) = %d\n" result_pos.x result_pos.y keypad.(result_pos.y).(result_pos.x)
         )
