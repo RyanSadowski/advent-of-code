dd41882198b25e8073f42b111f48ff6542126dd6
Ryan Sadowski <ryan.sadowski@knighttrans.com>
2023-09-03 13:48:38 -0700

    🙃

---
+++ ocaml/2016/day_04/bin/main.ml
@@ -0,0 +1,19 @@
+open Base
+open Stdio
+
+let () = print_endline "Hello, AOC 2016 Day 4"
+
+let read_file_to_lines filepath = 
+        let ic = In_channel.create filepath in 
+        let lines = In_channel.input_lines ic in
+        In_channel.close ic;
+        lines
+
+let () = 
+        let argv = Sys.get_argv () in
+        if Array.length argv < 2 then
+                print_endline "Please provide a filename"
+        else
+                let filename = argv.(1) in
+                let lines = read_file_to_lines filename in
+                printf "list length = %d\n" (List.length lines)
