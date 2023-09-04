5b9bebf51374c5ba5a2af3cc39c9f79f466ff58a
Ryan Sadowski <ryan.sadowski@knighttrans.com>
2023-09-03 19:41:23 -0700

    🙃

--- ocaml/2016/day_04/bin/main.ml
+++ ocaml/2016/day_04/bin/main.ml
@@ -15,6 +15,30 @@ let read_file_to_lines filepath =
         In_channel.close ic;
         lines
 
+let extract_between_brackets s =
+    match String.lsplit2 s ~on:'[' with
+    | Some (pre_bracket, post_bracket) ->
+        Some (pre_bracket, String.chop_suffix_exn post_bracket ~suffix:"]")
+    | None -> None
+
+let parse_string s =
+    match extract_between_brackets s with
+    | Some (main_content, bracket_content) ->
+        let parts = String.split main_content ~on:'-' in
+        begin
+            match List.rev parts with
+            | sector_str :: name_parts ->
+                let enc_name = String.concat ~sep:"-" (List.rev name_parts) in
+                let sector = Int.of_string sector_str in
+                let csum = bracket_content in
+                Some { enc_name; sector; csum }
+            | [] -> None
+        end
+    | None -> None
+
+let parse_lines lines =
+
+
 let () = 
         let argv = Sys.get_argv () in
         if Array.length argv < 2 then
